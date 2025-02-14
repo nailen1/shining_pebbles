ed2b05d57a94ea4b09f86ee0a4978bcd8d1dfe0d
JuneYoung <nietsnie1@gmail.com>
2025-02-14 14:38:23 +0900

    Add aws-s3-controller as a required dependency and bump version to 0.3.1

--- setup.py
+++ setup.py
@@ -1,29 +1,17 @@
-import os
-from setuptools import setup, find_packages, Extension
-import numpy
-
-
-extensions = [
-    Extension(
-        "shining_pebbles.some_extension",  # 확장 모듈 이름
-        sources=["shining_pebbles/some_extension.c"],  # 소스 파일
-        include_dirs=[numpy.get_include()],  # NumPy 헤더 포함
-    ),
-]
+from setuptools import setup, find_packages
 
 setup(
     name='shining_pebbles',
-    version='0.2.73',
+    version='0.3.1',
     packages=find_packages(),
      install_requires=[
         'numpy>=1.21.0',  # NumPy 1.x와 2.x 모두 지원
         'pandas',
         'python-dateutil',
         'pybind11>=2.12',
+        'aws-s3-controller>=0.7.3',
     ],
-    setup_requires=[
-        'numpy>=1.21.0',  # 빌드 시 NumPy 헤더 사용
-    ],
+
     author='June Young Park',
     author_email='juneyoungpaak@gmail.com',
     description='A collection of utility functions that enable treating a file system of multiple files as a pseudo-database, facilitating maintenance and operations across the large-scale file system. My shining pebbles.',
